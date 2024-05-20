54349c65c4f67a0cbc17b5bb2c56671a7b72f001
Yuuji Hasegawa <hasegawa@kamenwriter.com>
2024-05-20 17:42:47 +0900

    config edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,7 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	//output: 'export',
-	experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),  // 必要に応じてパスを調整
-  },
-	webpack: function (config, { isServer }) {
-		if (!isServer) {
-      // クライアントビルド用の設定
-      config.cache = {
-        type: 'filesystem',
-        buildDependencies: {
-          config: [__filename],
-        },
-      };
-    }
+	webpack: function (config) {
 		config.module.rules.push({
 			test: /\.md$/,
 			use: "raw-loader",
