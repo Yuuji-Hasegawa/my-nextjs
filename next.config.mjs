2cb27d7f4e168c12390a5c1b2687c439628dbb3b
Yuuji Hasegawa <hasegawa@kamenwriter.com>
2024-05-20 23:08:20 +0900

    output edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	//output: 'export',
+	output: 'export',
 	webpack: function (config) {
 		config.module.rules.push({
 			test: /\.md$/,
