c0e586a384c5fc067c5362c7b9f772e47008404c
Yuuji Hasegawa <hasegawa@kamenwriter.com>
2024-05-21 14:56:52 +0900

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	//output: 'export',
-	/*
 	experimental: {
     workerThreads: false,
     cpus: 1
   },
-	*/
 	webpack: function (config) {
 		config.module.rules.push({
 			test: /\.md$/,
